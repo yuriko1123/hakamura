3db4558f10169e585ccc79c1cf0e2009b12d82ea
kubo.y <>
2018-07-28 19:35:20 +0900

    お気に入りのurlを取得できるように修正しました

--- get-favo.js
+++ get-favo.js
@@ -1,11 +1,18 @@
 // twitterモジュールを読み込み
 var twitter = require('twitter');
-var fs = require("fs");
+var fs = require('fs');
 
-var client = new twitter(JSON.parse(fs.readFileSync("app.json","utf-8")));
+var client = new twitter(JSON.parse(fs.readFileSync('app.json', 'utf-8')));
 
 // ファボ取得
-client.get('favorites/list', function(error, tweets, response) {
+client.get('favorites/list', function(error, tweets) {
   if(error) throw error;
-  console.log(tweets[1].text);  
+  tweets.forEach(function(tweet) {
+    var favo = tweet.text; 
+
+    // urlのみを取得
+    var reg = new RegExp('http(s)?://([\\w-]+\.)+[\\w-]+(/[\\w-./?%&=]*)?', 'gi');
+    var mat = favo.match(reg);
+    console.log(mat[0]);
+  });
 });
